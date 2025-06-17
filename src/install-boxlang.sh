82b3830fe65c06f1e236f67f914c908263f93bfa
Luis Majano <lmajano@gmail.com>
2025-06-17 14:45:51 +0200

    more tests

--- src/install-boxlang.sh
+++ src/install-boxlang.sh
@@ -33,7 +33,6 @@ else
 
 	if curl -fsSL "$helpers_url" -o "$helpers_file"; then
 		chmod +x "$helpers_file"
-		cat "$helpers_file"
 		source "$helpers_file"
 		printf "${GREEN}✅ Helper functions downloaded successfully${NORMAL}\n"
 	else
